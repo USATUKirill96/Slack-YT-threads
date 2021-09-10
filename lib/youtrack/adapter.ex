6c6cbd1f9db7453fdabf824408848517d6c8fc0c
Gorill <kirill.ivanov96@gmail.com>
2021-09-10 14:18:57 +0500

    Обернул ссылку на обсуждение описанием

--- lib/youtrack/adapter.ex
+++ lib/youtrack/adapter.ex
@@ -2,21 +2,44 @@ defmodule Slackyt.YouTrack.Adapter do
   @base_path Application.get_env(:slackyt, :secret)[:base_path]
   @token Application.get_env(:slackyt, :secret)[:youtrack_token]
   @threads_field Application.get_env(:slackyt, :youtrack)[:threads_field]
+  @slack_url Application.get_env(:slackyt, :secret)[:slack_url]
 
+  @doc """
+  Добавить список тредов карточки задачи в ютреке
+  """
+  @spec update_threads(%{
+          :channel => binary,
+          :task => binary,
+          :thread => binary,
+          :description => binary
+        }) :: any()
   def update_threads(message) do
     thread_url =
-      "https://smenateam.slack.com/archives/" <> message.channel <> "/p" <> message.thread
+      (@slack_url <> message.channel <> "/p" <> message.thread)
+      |> wrap_url(message.description)
 
     message.task
     |> get_custom_fields()
+    # Выбрать только поле с тредами
     |> Enum.filter(fn field -> field["name"] == "Threads" end)
     |> List.first()
+    # Добавить строку с новым тредом
     |> then(fn field -> field["value"]["text"] <> "\n - " <> thread_url end)
     |> update_custom_fields(message.task)
   end
 
+  @spec wrap_url(binary, binary) :: binary
+  defp wrap_url(thread_url, description) do
+    # Обернуть УРЛ описанием задачи для красивого форматирования в ютреке
+    if description do
+      "[#{description}](#{thread_url})"
+    else
+      thread_url
+    end
+  end
+
   @doc """
-  Запросить список полей таски по названию или айди. Пример названия: SERVICES-1489
+  Запросить список кастомных полей таски по названию или айди. Пример названия: SERVICES-1489
   """
   @spec get_custom_fields(binary) :: map()
   def get_custom_fields(issue_id) do
@@ -29,6 +52,11 @@ defmodule Slackyt.YouTrack.Adapter do
     Poison.decode!(response.body)
   end
 
+  @doc """
+  Обновить значение поля "треды" в карточке
+  value: новое значение поля
+  issue_id: айди задачи
+  """
   @spec update_custom_fields(binary, binary) :: map()
   def update_custom_fields(value, issue_id) do
     body =
