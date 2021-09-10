485df583073d0b963272a90d99b88780ec5c17aa
Gorill <kirill.ivanov96@gmail.com>
2021-09-10 14:20:34 +0500

    Добавил пример локального конфига

---
+++ config/secret.example.exs
@@ -0,0 +1,8 @@
+# use Mix.Config
+
+# config :slackyt, :secret,
+#   youtrack_token: "youtrack_token_here",
+#   base_path: "https://youtrack.company.space/api/",
+#   slack_url: "https://yourteam.slack.com/archives/"
+
+# config :slack, api_token: "slack_token_here"
